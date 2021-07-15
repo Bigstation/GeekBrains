3b6a30e238569f807e59c196db37d5a851ea5f00
bigstation <av@unetg.ru>
2021-07-15 17:44:39 +0500

    test subprocess

---
+++ BD_PyQT/lesson1.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Выполнить простую системную команду с помощью os.system()
+# ret = subprocess.call("ls -l", shell=True)
+
+# Выполнить простую команду, игнорируя все, что она выводит
+#ret = subprocess.call("rm -f *.tmp", shell=True, stdout=open("/dev/null"))
